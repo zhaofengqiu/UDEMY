619718e04d7853d7da8e6ef2b824d9d764098293
zhaofengqiu <36105719+zhaofengqiu@users.noreply.github.com>
2018-12-30 14:27:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ HEADERS={"Authorization":Auto,
          "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"}
 BASEPATH = r" "
 THREAD_NUM = 5
+DOWNLOADSUMS = 1
